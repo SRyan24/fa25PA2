88944d09b18b7eead0893a18e7eeb28bc129cd02
Seamus Ryan <sryan7654@sdsu.edu>
2025-11-05 14:51:46 -0800

    Began heap.h, upheap() & downheap()

--- heap.h
+++ heap.h
@@ -16,20 +16,57 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
+
         return -1; // placeholder
     }
 
-    void upheap(int pos, int weightArr[]) {
+    void upheap(int pos, const int weightArr[]) { // CHANGED weightArr[] to const
         // TODO: swap child upward while smaller than parent
+
+        //Continuous upheap until sorted
+        while (pos >0) {
+            // ( i - 1 ) /2 : index formula
+            int parent = (pos - 1)/2;
+
+            // check if child is smaller, swap pos with parent if necessary to maintain order
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                swap(data[pos], data[parent]);
+                pos = parent;
+            }
+        }
     }
 
-    void downheap(int pos, int weightArr[]) {
+    void downheap(int pos, const int weightArr[]) { // CHANGED weightArr[] to const
         // TODO: swap parent downward while larger than any child
+        while (true) {
+            // (i*2)+(1 or 2) Child position formula
+            int childL = (pos*2)+1;
+            int childR = (pos*2)+2;
+            int smallest = pos;
+
+            //Find which child is smaller, assign to smallest
+            if (weightArr[data[childL]] < weightArr[data[smallest]]) {
+                smallest = childL;
+            }
+            if (weightArr[data[childR]] < weightArr[data[smallest]]) {
+                smallest = childR;
+            }
+
+            // if neither is smaller, swap position regardless, else finished
+            if (smallest != pos) {
+                swap(data[pos], data[smallest]);
+                pos = smallest;
+            } else {
+                break;
+            }
+
+        }
     }
 };
 
